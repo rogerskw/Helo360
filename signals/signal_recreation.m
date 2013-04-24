939a7c7a622f884ef89ec08daf663b8b779e9c66
Kyle Rogers <rogerskw@miamioh.edu>
2013-04-23 21:15:56 -0400

    signal_recreation.m properly recreates the signal for max_0_0

--- signals/signal_recreation.m
+++ signals/signal_recreation.m
@@ -1,7 +1,7 @@
 on = [ones(1,33)*1.5 ones(1,15)*(-4)];
 off = [ones(1,13)*1.5 ones(1,35)*(-4)];
 header = ones(1,100)*-4;
-wait = zeros(1,5200);
+wait = zeros(1,5199);
 
 max_alt = [header on on off on on on on on on off off off on off off off off on off off off off off off off off off  off on off on off];
 max_alt_with_wait = [max_alt wait];
@@ -29,4 +29,4 @@ plot(t1,x(x_signal_start:x_signal_start+length(t1)-1),t1,s(80000:length(s)))
 
 wavwrite(s./100,44100,'max_0_0_sim.wav');
 
-%sound(s,44100);
+sound(s,44100);
