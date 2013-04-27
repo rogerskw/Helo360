6217b79a62b64e9e45989ae61a03b2da9ec18f48
Kyle Rogers <rogerskw@miamioh.edu>
2013-04-27 11:19:35 -0400

    Implemented checksum function and create_audiosig function to convert array of 0's and 1's to wav file signal

--- helicopter.py
+++ helicopter.py
@@ -1,22 +1,60 @@
 import pygame
 
+
 # These values all relate to playing the signal at 44100 Hz
 header = [-4]*100
 wait = [0]*5199
-on = [1.5]*33 + [-4]*15
-off = [1.5]*13 + [-4]*35
+on = [2]*33 + [-4]*15
+off = [2]*13 + [-4]*35
 sig = on*32
 
-def add_header_wait(sig):
-    s = header + sig + wait
-    return s*100
+def add_header_wait(audiosig):
+    s = header + audiosig + wait
+    return s
+
+def create_audiosig(binary_sig):
+    sig = []
+    for bit in binary_sig:
+        b = on
+        if (bit == 0):
+            b = off
+        sig = sig + b
+    return sig
+
+def checksum(binary_sig):
+    sig_bytes = [binary_sig[0:8], binary_sig[8:16], binary_sig[16:24]]
+    carry = 0
+    check = [0]*8
+    for bit_num in reversed(xrange(8)):
+        c = carry + sig_bytes[0][bit_num] + sig_bytes[1][bit_num] + sig_bytes[2][bit_num]
+        carry = c / 2
+        check[bit_num] = c % 2
+    while carry != 0:
+        for bit_num in reversed(xrange(8)):
+            c = carry + check[bit_num]
+            carry = c / 2
+            check[bit_num] = c % 2
+            if carry == 0:
+                break
+    return check[:4]
+'''
+def throttle(trigger_val):
+    print 5
+
+x = [1,1,0,1,1,1,1,1,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
+x = [1]*28
+c = checksum(x)
+
+sig = create_audiosig(x+c)
+full_sig = add_header_wait(sig)
+print len(sig)
+print len(full_sig)
+
 
 if __name__ == '__main__':
-    # Initialize pygame stuff
     pygame.init()
     pygame.mixer.init()
     pygame.joystick.init()
-
     x360 = pygame.joystick.Joystick(0)
     x360.init()
 
@@ -29,3 +67,5 @@ if __name__ == '__main__':
         print throttle
         print x_axis
         print y_axis
+
+'''
