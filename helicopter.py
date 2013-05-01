a058f369bd0057f321c288f3b7f038aff8f9605d
Kyle Rogers <rogerskw@miamioh.edu>
2013-05-01 13:27:51 -0400

    Fixed frequency issue. Numpy array initiated at datatype int16 rather than int64 (default)

--- helicopter.py
+++ helicopter.py
@@ -1,19 +1,22 @@
 import pygame
 import numpy
-import matplotlib.pyplot as plt
 import sys
-#from datetime import datetime
+
+bits_per_byte = 8
 
 # These values all relate to playing the signal at 44100 Hz
 header_len = 100
 bit_len = 48
 wait_len = 5199
 bits_per_sig = 32
+up_amp = 40000
+down_amp = -40000
+empty_amp = 0
 
-header = [-40000]*header_len
-wait = [0]*wait_len
-on = [100000]*33 + [-40000]*15
-off = [100000]*13 + [-40000]*35
+header = [down_amp]*header_len
+wait = [empty_amp]*wait_len
+on = [up_amp]*33 + [down_amp]*15
+off = [up_amp]*13 + [down_amp]*35
 sig = header + on*bits_per_sig + wait
 
 bin_sig = [0]*bits_per_sig
@@ -59,9 +62,9 @@ def num2binary(val,n, val_min, val_max):
 
 # Calculates the checksum for the binary_sig according to the Helo TC helicopter
 def checksum(binary_sig):
-    sig_bytes = [binary_sig[0:8], binary_sig[8:16], binary_sig[16:24]]
+    sig_bytes = [binary_sig[0:bits_per_byte], binary_sig[bits_per_byte:2*bits_per_byte], binary_sig[2*bits_per_byte:3*bits_per_byte]]
     carry = 0
-    check = [0]*8
+    check = [0]*bits_per_byte
     for bit_num in reversed(xrange(8)):
         c = carry + sig_bytes[0][bit_num] + sig_bytes[1][bit_num] + sig_bytes[2][bit_num]
         carry = c / 2
@@ -81,49 +84,51 @@ def throttle(trigger_val):
 
 # This just substitues in standard values for the signal that is not throttle
 # or checksum. These values should be zero x,y movement.
-def restofsignal():
+def restofsignal(x_axis_val=0, y_axis_val=0):
     return [1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
+
+def create_bin_sig(throttle_val, x_axis_val, y_axis_val):
+    bin_sig[0:8] = throttle(throttle_val)
+    bin_sig[8:28] = restofsignal(x_axis_val,y_axis_val)
+    bin_sig[28:32] = checksum(bin_sig)
 
 def test_script(t):
     bin_sig[0:8] = throttle(t)
     bin_sig[8:28] = restofsignal()
     bin_sig[28:32] = checksum(bin_sig)
 
-    print bin_sig
     sig = create_audiosig(bin_sig)*60
-    print len(sig)
-    pygame.mixer.init(44100,-16,1,2**16)
-    num_ary = numpy.array(sig)
-    plt.plot(num_ary)
-    plt.savefig('generated_signal.jpg')
 
+    pygame.mixer.init(44100,-16,1,2**16)
+
+    num_ary = numpy.array(sig,dtype=numpy.dtype('int16'))
     if (sys.argv[1] == 'on'):
         s = pygame.sndarray.make_sound(num_ary)
         s.play()
 
-    plt.show()
+    while (pygame.mixer.get_busy()):
+        None
 
 if __name__ == '__main__':
     test_script(float(sys.argv[2]))
 
 '''
 
-
 if __name__ == '__main__':
-pygame.init()
-pygame.mixer.init()
-pygame.joystick.init()
-x360 = pygame.joystick.Joystick(0)
-x360.init()
+    pygame.init()
+    pygame.mixer.init()
+    pygame.joystick.init()
+    x360 = pygame.joystick.Joystick(0)
+    x360.init()
 
-while True:
-    pygame.event.wait() # Wait for something to happen before doing anything
-    # Left stick is axis 0 and 1, right trigger is axis 5
-    x_axis = x360.get_axis(0)
-    y_axis = x360.get_axis(1)
-    throttle = x360.get_axis(5)
-    print throttle
-    print x_axis
-    print y_axis
+    while True:
+        pygame.event.wait() # Wait for something to happen before doing anything
+        # Left stick is axis 0 and 1, right trigger is axis 5
+        x_axis = x360.get_axis(0)
+        y_axis = x360.get_axis(1)
+        throttle = x360.get_axis(5)
+        print throttle
+        print x_axis
+        print y_axis
 
 '''
